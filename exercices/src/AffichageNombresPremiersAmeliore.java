f32370e2e9d08a435211bce279e0f5ff6a1feeff
Severin Nandillon <nandillons@gmail.com>
2020-11-09 20:02:33 +0100

    Exercice : afficher les nombres premiers version 2

--- exercices/src/AffichageNombresPremiersAmeliore.java
+++ exercices/src/AffichageNombresPremiersAmeliore.java
@@ -2,16 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AffichageNombresPremiersAmeliore {
-
 	public static void main(String[] args) {
 		List<Integer> nombresPremiers = new ArrayList<>();
 		int nbNombresPremiers = 0;
 		
 		for (int i = 2; i <= 1000; i++) {
 			boolean estUnNombrePremier = true;
-			for (int j = 2; j < i; j++) {
+			for (int j : nombresPremiers) {
 				if (i % j == 0) {
-					// i n'est pas un nombre premier
 					estUnNombrePremier = false;
 					break;
 				}
