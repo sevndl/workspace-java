6bb74a85289858d1c49e7cd4b2282b9390b06a31
Severin Nandillon <nandillons@gmail.com>
2020-11-09 20:17:25 +0100

    ajout de string

--- exercices/src/Phrase.java
+++ exercices/src/Phrase.java
@@ -1,11 +1,12 @@
-import java.lang.*;
-
 public class Phrase {
-  public String phrase = "Bonjour le monde";
+  
+  private String phrase = "";
 
-  // public String ajouter(String valeur) {
-  //   return phrase.concat(valeur);
-  // }
+  public String ajouter(String aAjouter) {
+    phrase = phrase + aAjouter + " ";
+    System.out.println(phrase);
+    return phrase;
+  }
 
   public int getNbLettres() {
     int cpt = 0;
@@ -19,11 +20,19 @@ public class Phrase {
   }
 
   public static void main(String[] args) {
-    Phrase phrase1 = new Phrase();
+    Phrase phrase = new Phrase();
+    
+    phrase.ajouter("Une");
+    phrase.ajouter("classe");
+    phrase.ajouter("pour");
+    phrase.ajouter("ajouter");
+    phrase.ajouter("des mots");
+    // phrase.setSeparateur(" et encore ");
+    // phrase.ajouter("des mots", 3);
+    // phrase.setSeparateur(' ');
+    // phrase.ajouter("toujours", "et", "encore");
 
-    // phrase1.ajouter("Salut la moule");
-
-    System.out.println(phrase1);
-    System.out.println(phrase1.getNbLettres());
+    System.out.println(phrase);
+    System.out.println(phrase.getNbLettres());
   }
 }
