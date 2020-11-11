5c1db30816146aa37b3ddc455681ae365d610874
Severin Nandillon <nandillons@gmail.com>
2020-11-11 12:09:25 +0100

    mot masqueé

--- exercices/src/JeuDuPendu.java
+++ exercices/src/JeuDuPendu.java
@@ -1,26 +1,24 @@
 import java.util.Scanner;
 
 public class JeuDuPendu {
-  private char [] mot;
-  private char [] motMasque;
+  private String mot;
+  private String motMasque;
   private int nbTentatives = 6;
-  private boolean estTrouve = false;
 
   public JeuDuPendu(String aDeviner) {
-    String aDevinerMinuscules = aDeviner.toLowerCase();
-    this.mot = aDevinerMinuscules.toCharArray();
-    for (char c : this.motMasque) {
-      this.motMasque[c] = '_';
-    }
+    mot = aDeviner.toLowerCase();
+    motMasque = mot;
+    motMasque = motMasque.replaceAll("[a-z]", "_");
   }
 
   public boolean proposer(char lettreProposee) {
     int index = 0;
     int occurences = 0;
 
-    for (char c : this.mot) {
+    for (char c : mot.toCharArray()) {
       if (lettreProposee == c) {
-        this.motMasque[index] = lettreProposee;
+        char lettreActuelle = motMasque.charAt(index);
+        motMasque = motMasque.replace(lettreActuelle, lettreProposee);
         occurences += 1;
       }
       index += 1;
@@ -28,58 +26,71 @@ public class JeuDuPendu {
     if (occurences >= 1) {
       return true;
     } else {
-      this.nbTentatives -= 1;
+      nbTentatives -= 1;
       return false;
     }
   }
 
   public int getNbTentativesRestantes() {
-    return this.nbTentatives;
+    return nbTentatives;
   }
 
-  // public boolean isPartieGagnee() {
-    
-  // }
+  public boolean isPartieGagnee() {
+    for (char c : motMasque.toCharArray()) {
+      if (c == '_') {
+        return false;
+      }
+    }
+    if (nbTentatives > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-  // public boolean isPartieTerminee() {
-
-  // }
+  public boolean isPartieTerminee() {
+    if (nbTentatives == 0 || isPartieGagnee()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   public String getMotMasque() {
-    String motMasqueString = this.motMasque.toString();
+    String motMasqueString = motMasque.toString();
     return motMasqueString;
   }
 
   public String getSolution() {
-    String motString = this.mot.toString();
+    String motString = mot.toString();
     return motString;
   }
 
 
-  // public static void main(String[] args) {
-	// 	Scanner scanner = new Scanner(System.in);
-	// 	JeuDuPendu jeuDuPendu = new JeuDuPendu("feuille");
+  public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		JeuDuPendu jeuDuPendu = new JeuDuPendu("feuille");
 
-	// 	do {
-	// 		String motMasque = jeuDuPendu.getMotMasque();
-	// 		int nbTentativesRestantes = jeuDuPendu.getNbTentativesRestantes();
-	// 		System.out.printf("Le mot : %s\n", motMasque);
-	// 		System.out.printf("Proposez une lettre (il reste %d tentatives) : ", nbTentativesRestantes);
-	// 		String ligne = scanner.nextLine();
-	// 		if (ligne.length() != 1) {
-	// 			System.out.println("Vous devez saisir une seule lettre!");
-	// 			continue;
-	// 		}
-	// 		char lettre = ligne.charAt(0);
-	// 		if (! jeuDuPendu.proposer(lettre)) {
-	// 			System.out.println("Cette lettre n'est pas présente dans le mot à trouver.");
-	// 		}
-	// 	} while(! jeuDuPendu.isPartieTerminee());
+		do {
+			String motMasque = jeuDuPendu.getMotMasque();
+			int nbTentativesRestantes = jeuDuPendu.getNbTentativesRestantes();
+			System.out.printf("Le mot : %s\n", motMasque);
+			System.out.printf("Proposez une lettre (il reste %d tentatives) : ", nbTentativesRestantes);
+			String ligne = scanner.nextLine();
+			if (ligne.length() != 1) {
+				System.out.println("Vous devez saisir une seule lettre!");
+				continue;
+			}
+			char lettre = ligne.charAt(0);
+			if (! jeuDuPendu.proposer(lettre)) {
+				System.out.println("Cette lettre n'est pas présente dans le mot à trouver.");
+			}
+		} while(! jeuDuPendu.isPartieTerminee());
 
-	// 	if (jeuDuPendu.isPartieGagnee()) {
-	// 		System.out.println("Bravo, vous avez gagné !");
-	// 	} else {
-	// 		System.out.printf("Vous avez perdu. Le mot à trouver était %s.\n", jeuDuPendu.getSolution());
-	// 	}
-	// }
+		if (jeuDuPendu.isPartieGagnee()) {
+			System.out.println("Bravo, vous avez gagné !");
+		} else {
+			System.out.printf("Vous avez perdu. Le mot à trouver était %s.\n", jeuDuPendu.getSolution());
+		}
+	}
 }
