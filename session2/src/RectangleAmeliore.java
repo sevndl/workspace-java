fbd1b0eec6a42b95fb924304b845dd45f74af536
Severin Nandillon <nandillons@gmail.com>
2020-11-12 14:27:43 +0100

    session 2, exo 1 : rectangle amélioré => reprise code Rectangle.java

---
+++ session2/src/RectangleAmeliore.java
@@ -0,0 +1,44 @@
+public class RectangleAmeliore {
+  private double longueur;
+  private double largeur;
+
+  public double getLongueur() {
+    return this.longueur;
+  }
+
+  public void setLongueur(double valeur) {
+    this.longueur = valeur;
+  }
+
+  public double getLargeur() {
+    return this.largeur;
+  }
+
+  public void setLargeur(double valeur) {
+    this.largeur = valeur;
+  }
+
+  public double getPerimetre() {
+    return 2 * (this.largeur + this.longueur);
+  }
+
+  public double getAire() {
+    return this.largeur * this.longueur;
+  }
+
+  public boolean isCarre() {
+    if (this.largeur == this.longueur) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public void transpose() {
+    double temp;
+
+    temp = this.longueur;
+    this.longueur = this.largeur;
+    this.largeur = temp;
+  }
+}
