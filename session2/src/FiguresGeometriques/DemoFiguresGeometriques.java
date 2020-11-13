fcdc36c868042a0826a8c0c9482e344e1510461e
Severin Nandillon <nandillons@gmail.com>
2020-11-13 10:51:58 +0100

    Exercice figures geometriques 2/3

---
+++ session2/src/FiguresGeometriques/DemoFiguresGeometriques.java
@@ -0,0 +1,28 @@
+package FiguresGeometriques;
+
+public class DemoFiguresGeometriques {
+  public static void main(String[] args) {
+		FigureComposee f = new FigureComposee();
+		FigureComposee f2 = new FigureComposee();
+		f.ajouterFigure(new Rectangle(10, 20));
+		f.ajouterFigure(new Rectangle(5, 10));
+		f.ajouterFigure(new Disque(5));
+		System.out.println(f.getPerimetre());
+		System.out.println(f.getAire());
+		f2.ajouterFigure(new Rectangle(10, 20));
+		f2.ajouterFigure(new Rectangle(5, 10));
+		f2.ajouterFigure(new Disque(5));
+		f.ajouterFigure(f2);
+
+		System.out.println(f.getPerimetre());
+		System.out.println(f.getAire());
+
+		// Figure r = new Rectangle(10, 20);
+		// Figure d = new Disque(5);
+
+		// System.out.println(r.getAire());
+		// System.out.println(r.getPerimetre());
+		// System.out.println(d.getAire());
+		// System.out.println(d.getPerimetre());
+	}
+}
--- session2/src/FiguresGeometriques/Figure.java
+++ session2/src/FiguresGeometriques/Figure.java
@@ -8,14 +8,4 @@ public abstract class Figure {
 	public double getAire() {
 		return this.getAire();
 	}
-
-	public static void main(String[] args) {
-		Figure r = new Rectangle(10, 20);
-		Figure d = new Disque(5);
-
-		System.out.println(r.getAire());
-		System.out.println(r.getPerimetre());
-		System.out.println(d.getAire());
-		System.out.println(d.getPerimetre());
-	}
 }
