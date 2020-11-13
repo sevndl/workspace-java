77b12e42bcc515e7c70e1d888a98efe5d72306e3
Severin Nandillon <nandillons@gmail.com>
2020-11-13 11:51:43 +0100

    Exercice 3/3 : figures geometriques

--- session2/src/FiguresGeometriques/DemoFiguresGeometriques.java
+++ session2/src/FiguresGeometriques/DemoFiguresGeometriques.java
@@ -2,6 +2,19 @@ package FiguresGeometriques;
 
 public class DemoFiguresGeometriques {
   public static void main(String[] args) {
+		Figure r = new Rectangle(10, 20);
+		Figure r2 = new Rectangle(10, 20);
+		Figure d = new Disque(5);
+
+		System.out.println(r.getAire());
+		System.out.println(r.getPerimetre());
+		System.out.println(d.getAire());
+		System.out.println(d.getPerimetre());
+
+		if (r.equals(r2)) {
+			System.out.println("R = D");
+		}
+
 		FigureComposee f = new FigureComposee();
 		FigureComposee f2 = new FigureComposee();
 		f.ajouterFigure(new Rectangle(10, 20));
@@ -16,13 +29,5 @@ public class DemoFiguresGeometriques {
 
 		System.out.println(f.getPerimetre());
 		System.out.println(f.getAire());
-
-		// Figure r = new Rectangle(10, 20);
-		// Figure d = new Disque(5);
-
-		// System.out.println(r.getAire());
-		// System.out.println(r.getPerimetre());
-		// System.out.println(d.getAire());
-		// System.out.println(d.getPerimetre());
 	}
 }
--- session2/src/FiguresGeometriques/Figure.java
+++ session2/src/FiguresGeometriques/Figure.java
@@ -8,4 +8,14 @@ public abstract class Figure {
 	public double getAire() {
 		return this.getAire();
 	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (object instanceof Figure) {
+			Figure f = (Figure) object;
+			return (this.getAire() == f.getAire() && this.getPerimetre() == f.getPerimetre());
+		} else {
+			return false;
+		}
+	}
 }
