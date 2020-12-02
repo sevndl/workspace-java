419437d566efd949473998ed9397c59f8d0b3db7
Severin Nandillon <nandillons@gmail.com>
2020-12-02 20:54:59 +0100

    Ajout de l'interface ImporteurCatalogue et de la classe ImportCatalogue

---
+++ session5/src/fr/epsi/catalogue/io/ImportCatalogue.java
@@ -0,0 +1,16 @@
+package fr.epsi.catalogue.io;
+
+import java.io.IOException;
+
+import fr.epsi.catalogue.Catalogue;
+
+public class ImportCatalogue implements ImporteurCatalogue {
+
+	@Override
+	public Catalogue importer() throws IOException {
+		
+		return null;
+		
+	}
+
+}
---
+++ session5/src/fr/epsi/catalogue/io/ImporteurCatalogue.java
@@ -0,0 +1,10 @@
+package fr.epsi.catalogue.io;
+
+import java.io.IOException;
+import fr.epsi.catalogue.Catalogue;
+
+public interface ImporteurCatalogue {
+
+	Catalogue importer() throws IOException;
+
+}
