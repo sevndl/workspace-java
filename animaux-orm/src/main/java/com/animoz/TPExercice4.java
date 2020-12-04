2d05be241812afb02ec96b40948a338a5f4418c7
Severin Nandillon <nandillons@gmail.com>
2020-12-04 15:50:06 +0100

    TP Exercice 4

---
+++ animaux-orm/src/main/java/com/animoz/TPExercice4.java
@@ -0,0 +1,31 @@
+package com.animoz;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.animoz.modele.Espece;
+
+public class TPExercice4 {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("animaux");
+		EntityManager em = emf.createEntityManager();
+		
+		try {
+			
+			// il faut voir ça côté objet et pas côté SQL
+			List<Espece> especes = em.createQuery("select e from Espece e order by e.nom", Espece.class).getResultList();
+			for (Espece e : especes) {
+				System.out.println(e.getNom());
+			}
+			
+		} finally {
+			em.close();
+			emf.close();
+		}
+	}
+
+}
