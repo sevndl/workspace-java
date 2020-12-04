90669f00793255486c26bfa06d60c93679c5b427
Severin Nandillon <nandillons@gmail.com>
2020-12-04 17:24:39 +0100

    Update TP Exercice 7

--- animaux-orm/src/main/java/com/animoz/modele/Soigneur.java
+++ animaux-orm/src/main/java/com/animoz/modele/Soigneur.java
@@ -2,10 +2,13 @@ package com.animoz.modele;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Soigneur {
@@ -15,6 +18,9 @@ public class Soigneur {
 	private Long id;
 	private String numero;
 	private String nom;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(updatable=false)
 	private Date dateRecrutement;
 	
 	public String getNumero() {
