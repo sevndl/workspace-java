804618ce287c26fd900ed90e3c2153bf74a62eed
Severin Nandillon <nandillons@gmail.com>
2020-12-04 16:39:35 +0100

    TP exercice 6

---
+++ animaux-orm/src/main/java/com/animoz/TPExercice6.java
@@ -0,0 +1,42 @@
+package com.animoz;
+
+import java.util.List;
+import java.util.Scanner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.animoz.modele.Animal;
+
+public class TPExercice6 {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("animaux");
+		EntityManager em = emf.createEntityManager();
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Nom de l'animal recherché : ");
+		String nom = in.nextLine();
+		nom = nom.toLowerCase();
+		
+		try {
+			
+			List<Animal> animal = em.createQuery("select a from Animal a where LOWER(a.nom) = :nom", Animal.class)
+									.setParameter("nom", nom)
+									.getResultList();
+			for (Animal a : animal) {
+				System.out.println(a.getId());
+				System.out.println(a.getNom());
+				System.out.println(a.getOrigine());
+				System.out.println(a.getDescription());
+				System.out.println(a.getRegime());
+			}
+			
+		} finally {
+			em.close();
+			emf.close();
+		}
+	}
+
+}
