0169cd284afd7e3cd8c308f77a40c8c05be326ca
Severin Nandillon <nandillons@gmail.com>
2020-12-08 10:14:44 +0100

    classe Espece -> relation onetomany mise en place

--- animaux-orm/src/main/java/com/animoz/modele/Espece.java
+++ animaux-orm/src/main/java/com/animoz/modele/Espece.java
@@ -1,9 +1,12 @@
 package com.animoz.modele;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Espece {
@@ -12,6 +15,17 @@ public class Espece {
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // pour préciser à hibernate que c'est en auto-increment
 	private Long id;
 	private String nom;
+	
+	@OneToMany
+	private List<Animal> animaux;
+	
+	public List<Animal> getAnimaux() {
+		return this.animaux;
+	}
+	
+	public void setAnimaux(List<Animal> animaux) {
+		this.animaux = animaux;
+	}
 
 	public Long getId() {
 		return id;
