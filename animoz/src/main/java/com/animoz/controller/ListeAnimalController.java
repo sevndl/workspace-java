6699f79be210e5d729aa5fd11ad4773f7468e2fa
Severin Nandillon <nandillons@gmail.com>
2020-12-11 12:26:44 +0100

    dynamisation de la vue html

--- animoz/src/main/java/com/animoz/controller/ListeAnimalController.java
+++ animoz/src/main/java/com/animoz/controller/ListeAnimalController.java
@@ -1,13 +1,15 @@
 package com.animoz.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ListeAnimalController {
 
 	@GetMapping(path = "/animaux") // anotation qui permet de dire que c'est une méthode HTTP GET
-	public String afficherListeAnimaux() {
+	public String afficherListeAnimaux(Model model) {
+		model.addAttribute("leNombre", 42); // avec thymeleaf on peut dynamiser la page
 		return "listeAnimaux"; // listeAnimaux est le nom de la vue, du template (la page html avec des instructions dynamiques)
 	}
 
