ee388af9385fe1dc9bf4c7745284a2d88c0e56dc
Severin Nandillon <nandillons@gmail.com>
2020-12-11 09:19:27 +0100

    animoz EE initial commit

---
+++ animoz/src/main/java/com/animoz/AnimozApplication.java
@@ -0,0 +1,13 @@
+package com.animoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimozApplication.class, args);
+	}
+
+}
