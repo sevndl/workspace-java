641194f913c2259c5fe8bf56e0f10d4438a48b6d
Severin Nandillon <nandillons@gmail.com>
2020-12-11 11:58:12 +0100

    création de la page html liée au controller et à la route /animaux

--- animoz/src/main/java/com/animoz/controller/ListeAnimalController.java
+++ animoz/src/main/java/com/animoz/controller/ListeAnimalController.java
@@ -1,12 +1,14 @@
 package com.animoz.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ListeAnimalController {
 
+	@GetMapping(path = "/animaux") // anotation qui permet de dire que c'est une méthode HTTP GET
 	public String afficherListeAnimaux() {
-		return "listeAnimaux";
+		return "listeAnimaux"; // listeAnimaux est le nom de la vue, du template (la page html avec des instructions dynamiques)
 	}
 
 }
