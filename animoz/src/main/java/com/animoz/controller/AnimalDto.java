30368725691f667f66d8fe8c5bf87f3bada714a8
Severin Nandillon <nandillons@gmail.com>
2020-12-15 10:50:06 +0100

    Create AnimalDto.java

---
+++ animoz/src/main/java/com/animoz/controller/AnimalDto.java
@@ -0,0 +1,17 @@
+package com.animoz.controller;
+
+public class AnimalDto {
+
+	private String nom;
+
+	public String getNom() {
+		return this.nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+
+}
