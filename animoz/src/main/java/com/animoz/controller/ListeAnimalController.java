52e3a6a027052e478cbf2e3fea5b78398a6792a5
Severin Nandillon <nandillons@gmail.com>
2020-12-15 10:50:31 +0100

    Update ListeAnimalController.java

--- animoz/src/main/java/com/animoz/controller/ListeAnimalController.java
+++ animoz/src/main/java/com/animoz/controller/ListeAnimalController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.animoz.modele.Animal;
 import com.animoz.service.AnimalService;
@@ -18,10 +20,25 @@ public class ListeAnimalController {
 
 	@GetMapping("/animaux")
 	public String afficherListeAnimaux(Model model) {
-		model.addAttribute("leNombre", 42);
 		List<Animal> animaux = animalService.getListeAnimaux();
 		model.addAttribute("listeAnimaux", animaux);
 		return "listeAnimaux";
+	}
+	
+	@GetMapping("/animal")
+	// demande à Spring un param de type AnimalDto -> @ModelAttribute c'est un truc qui doit être dans le model, s'il n'y est pas crée-le
+	public String afficherFormulaireAnimal(@ModelAttribute("animal") AnimalDto animal) {
+//		animal.setNom("sev"); // on peut ainsi pré-remplir le formulaire
+		return "formulaireAnimal";
+	}
+	
+	@PostMapping("/animal")
+	public String traiterFormulaireAnimal(@ModelAttribute("animal") AnimalDto animal) {
+		// TODO traiter le formulaire
+//		return "succesCreationAnimal"; // à créer
+		
+		System.out.println(animal.getNom());
+		return "formulaireAnimal"; // test
 	}
 
 }
