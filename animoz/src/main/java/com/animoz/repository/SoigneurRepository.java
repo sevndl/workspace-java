d470149d054c8699379c68610bcc17fa1547039f
Severin Nandillon <nandillons@gmail.com>
2020-12-15 10:51:13 +0100

    Create SoigneurRepository.java

---
+++ animoz/src/main/java/com/animoz/repository/SoigneurRepository.java
@@ -0,0 +1,23 @@
+package com.animoz.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.animoz.modele.Soigneur;
+
+@Repository
+public class SoigneurRepository {
+
+	@Autowired
+	private EntityManager em;
+	
+	public List<Soigneur> getListeSoigneurs() {
+		return em.createQuery("select s from Soigneur s", Soigneur.class)
+				 .getResultList();
+	}
+
+}
