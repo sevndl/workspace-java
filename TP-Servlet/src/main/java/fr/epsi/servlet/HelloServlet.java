da64394a9bc252af1b3827f01a2afc74f213bbf4
Severin Nandillon <nandillons@gmail.com>
2021-01-20 12:25:32 +0100

    mise en place d'un writer

--- TP-Servlet/src/main/java/fr/epsi/servlet/HelloServlet.java
+++ TP-Servlet/src/main/java/fr/epsi/servlet/HelloServlet.java
@@ -1,6 +1,7 @@
 package fr.epsi.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,10 +10,16 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
 
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-	        throws ServletException, IOException
-	    {
-	        System.out.println("Bonjour !");
-	    }
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	        PrintWriter out = resp.getWriter();
+	        out.write("<html>");
+	        out.write("<head>");
+	        out.write("<title>Web app</title>");
+	        out.write("</head>");
+	        out.write("<body>");
+	        out.write("<h3 style='color:red'>Titre</h3>");
+	        out.write("</body>");
+	        out.write("</html>");
+	}
 	
 }
