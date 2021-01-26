fe1bb563290dc0148a941f34cbdd8967e716bd22
Severin Nandillon <nandillons@gmail.com>
2021-01-26 14:43:47 +0100

    annotations au lieu de web.xml

--- TP-Servlet/src/main/java/fr/epsi/servlet/HelloServlet.java
+++ TP-Servlet/src/main/java/fr/epsi/servlet/HelloServlet.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// si on veut éviter d'écrire dans un fichier .xml => il remplace tout le contenu de web.xml
+@WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
