81ce533adcc26aae885c0618c1cee342087c06ff
Severin Nandillon <nandillons@gmail.com>
2021-01-26 14:56:02 +0100

    introduction au jsp pour utiliser du html

--- TP-Servlet/src/main/java/fr/epsi/servlet/HelloServlet.java
+++ TP-Servlet/src/main/java/fr/epsi/servlet/HelloServlet.java
@@ -1,7 +1,6 @@
 package fr.epsi.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,24 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// si on veut éviter d'écrire dans un fichier .xml => il remplace tout le contenu de web.xml
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		int idClient = Integer.parseInt(req.getParameter("id"));
-		System.out.println(idClient);
-		
-        PrintWriter out = resp.getWriter();
-        out.write("<html>");
-        out.write("<head>");
-        out.write("<title>Web app</title>");
-        out.write("</head>");
-        out.write("<body>");
-        out.write("<h3 style='color:red'>Titre</h3>");
-        out.write("<p>Bonjour client n°" + idClient + "</p>");
-        out.write("</body>");
-        out.write("</html>");
+		this.getServletContext().getRequestDispatcher("/WEB-INF/pages/hello.jsp").forward(req, resp);
 	}
 	
 }
