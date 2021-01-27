f60db1310bb99f9faa3583bf14a6bdd9e2336135
Severin Nandillon <nandillons@gmail.com>
2021-01-27 17:58:02 +0100

    mise en pratique du dao

--- TP-Servlet/src/main/java/fr/epsi/service/ProduitService.java
+++ TP-Servlet/src/main/java/fr/epsi/service/ProduitService.java
@@ -1,19 +1,31 @@
 package fr.epsi.service;
 
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 
+import fr.epsi.dao.IProduitDao;
+import fr.epsi.dao.ProduitDao;
 import fr.epsi.entite.Produit;
 
+//CONTAINER -> transactions par le serveur d'application => pas utile ici
 @Stateless
+@TransactionManagement(TransactionManagementType.BEAN)
 public class ProduitService implements IProduitService {
 
 	@PersistenceContext
 	private EntityManager em;
+
+	@Resource
+	private UserTransaction utx;
 	
 	public void createProduit(Produit p) {
-		em.persist(p);
+		IProduitDao dao = new ProduitDao(em, utx);
+		dao.create(p);
 	}
 
 }
