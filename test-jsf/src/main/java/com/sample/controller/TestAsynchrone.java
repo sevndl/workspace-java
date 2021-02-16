67574c2b9b7a9ff358ed5358a159b754e59f1f50
Severin Nandillon <nandillons@gmail.com>
2021-02-16 09:44:19 +0100

    test jsf + asynchrone

---
+++ test-jsf/src/main/java/com/sample/controller/TestAsynchrone.java
@@ -0,0 +1,30 @@
+package com.sample.controller;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class TestAsynchrone {
+	
+	public static void main(String[] args) {
+		ConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
+		try {
+			Connection connection = factory.createConnection();
+			Session session = connection.createSession(false,  Session.AUTO_ACKNOWLEDGE);
+			Destination queue = session.createQueue("Queue EPSI");
+			TextMessage message = session.createTextMessage("hello world");
+			MessageProducer producer = session.createProducer(queue);
+			producer.send(message);
+			session.close();
+			connection.close();
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}		
+	}
+	
+}
